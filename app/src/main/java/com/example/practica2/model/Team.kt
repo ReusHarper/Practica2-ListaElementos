0b95c360e16517c2bd1fb7dc8afdc16b014b738f
ReusHarper <reusharper@comunidad.unam.mx>
2022-11-03 00:49:17 -0600

    Creacion de actividades restantes

--- app/src/main/java/com/example/practica2/model/Team.kt
+++ app/src/main/java/com/example/practica2/model/Team.kt
@@ -1,15 +1,13 @@
 package com.example.practica2.model
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
 // Creacion de la entidad team
 //  @Entity(tableName = "team_table")
 data class Team (
     //@PrimaryKey(autoGenerate = true)
+    val id: Int,
     val name: String,
     val division: String,
     val conference: String,
-    val logo: String,
     val titles: Int,
+    val logo: String?,
 )
--- app/src/main/java/com/example/practica2/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/practica2/view/activities/MainActivity.kt
@@ -4,38 +4,112 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.practica2.databinding.ActivityMainBinding
+import com.example.practica2.db.DbTeams
 import com.example.practica2.model.Team
 import com.example.practica2.view.adapters.TeamAdapter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    //private var teamMutableList: MutableList<Team> = TeamsProvider.teamList.toMutableList()
+    private lateinit var listTeams: ArrayList<Team>
+
+    //private lateinit var adapter: TeamAdapter
+    private val llmanager = LinearLayoutManager(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initRecyclerView()
+        //binding.btnAddTeam.setOnClickListener { createTeam() }
+        //initRecyclerView()
+
+        val dbTeams = DbTeams(this)
+        listTeams = dbTeams.getTeams()
+
+        val teamsAdapter = TeamAdapter(this, listTeams)
+        binding.rvTeams.layoutManager = LinearLayoutManager(this)
+        binding.rvTeams.adapter = teamsAdapter
+
+        if(listTeams.size == 0) binding.tvSinRegistros.visibility = View.VISIBLE
+        else binding.tvSinRegistros.visibility = View.INVISIBLE
     }
 
+    fun click(view: View) {
+        startActivity(Intent(this, AddActivity::class.java))
+        finish()
+    }
+
+    fun selectedTeam(team: Team){
+        //Manejamos el click del elemento en el recycler view
+        val intent = Intent(this, DetailsActivity::class.java)
+        intent.putExtra("ID", team.id)
+        startActivity(intent)
+        finish()
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
     private fun initRecyclerView() {
+        // Se asigna la lista mutable al adapter para que modifique el contenido del recycler view
+        adapter = TeamAdapter(this, listTeams)
         val manager = LinearLayoutManager(this)
 
         // El LinearLayoutManager se encarga de mostrar los items de manera en forma de listado unilateral
         binding.rvTeams.layoutManager = manager
         // Pasamos los items creados desde el TeamsProvider al adapter
-        binding.rvTeams.adapter =
-            TeamAdapter(TeamsProvider.teamList) { team ->
-                onItemSelected(team)
-            }
+        binding.rvTeams.adapter = adapter
     }
 
-    fun onItemSelected(team: Team) {
+    private fun onItemSelected(team: Team) {
         Toast.makeText(
             this,
             team.name,
@@ -43,9 +117,20 @@ class MainActivity : AppCompatActivity() {
         ).show()
     }
 
-    fun click(view: View) {
+    private fun onItemDeleted(position: Int) {
+        // Borrado del item en la lista
+        teamMutableList.removeAt(position)
+
+        // Notificamos al recyclerview el item eliminado del listado
+        adapter.notifyItemRemoved(position)
+    }
+
+    // Se manda a llamar a la activity Add cuando se oprima el boton de agregar
+    fun onItemAdd(view: View) {
         startActivity(Intent(this, AddActivity::class.java))
         finish()
     }
+
+     */
 
 }
